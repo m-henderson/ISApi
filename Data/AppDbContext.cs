e7dd4d0409691301eeda6b03df2fa27a99151db7
Michael Henderson <mvhenderson.4@icloud.com>
2017-11-08 14:45:43 -0600

    add models

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ISApi.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISApi.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? SitNumber { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DateCreated { get; set; }
+        public int? MonthCreated { get; set; }
+        public int? YearCreated { get; set; }
+        public int StatusId { get; set; }
+        public bool IsCallBack { get; set; }
+        public virtual ICollection<Status> Statuses { get; set; }
+    }
+}
