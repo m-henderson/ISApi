0aa950c9da8210ddd9c76545df0ba9113b9baa37
Michael Henderson <mvhenderson.4@icloud.com>
2017-11-15 07:31:21 -0600

    fix api url

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace ISApi
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseKestrel()
-                .UseUrls("http://localhost:8080")
+                .UseUrls("http://192.168.0.32:8080")
                 .Build();
     }
 }
